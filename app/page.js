392865afa27b54e7748dc52396b85286474f053b
sneeraj.by <sneeraj.by@gmail.com>
2024-03-18 14:09:38 +0530

    Added loader,notfound and recent properties page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import RecentProperties from "@/components/RecentProperties";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <RecentProperties />
     </>
   );
 };
